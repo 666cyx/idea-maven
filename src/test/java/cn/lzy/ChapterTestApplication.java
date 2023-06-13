9bf7d0d52fef0d024a7261644aa116542dddc7dd
“666cyx” <2775254436@qq.com>
2023-06-13 21:48:45 +0800

    Changes

--- src/test/java/cn/lzy/ChapterTestApplication.java
+++ src/test/java/cn/lzy/ChapterTestApplication.java
@@ -1,5 +1,6 @@
 package cn.lzy;
 
+import cn.lzy.configbean.Student;
 import cn.lzy.controller.ChapterController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,12 +11,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChapterTestApplication {
-    private  String TAG="ChapterTestApplication";
+//    private  String TAG="ChapterTestApplication";
     @Autowired
     ChapterController chapterController;
+    @Autowired
+   Student student;
+
+
+    
     @Test
-    public void  getHello(){
-        String result=chapterController.sayHello();
-        System.out.println(TAG+"单元测试返回==="+result);
+    public void contextLoads(){
+        System.out.println("单元测试返回的数据--"+chapterController.sayHello());
+        System.out.println("单元测试返回的student数据--"+student);
     }
+//    public void  getHello(){
+//        String result=chapterController.sayHello();
+//        System.out.println(TAG+"单元测试返回==="+result);
+//    }
 }
