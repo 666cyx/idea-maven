aa33974ff9c793d5826c4e0b1b058460be7e947a
“666cyx” <2775254436@qq.com>
2023-09-07 16:31:48 +0800

    提交更新

--- src/test/java/cn/lzy/ChapterTestApplication.java
+++ src/test/java/cn/lzy/ChapterTestApplication.java
@@ -6,9 +6,14 @@ import cn.lzy.configbean.User;
 import cn.lzy.controller.ChapterController;
 import cn.lzy.customconfig.CustomProperties;
 import cn.lzy.customconfig.MyProperties;
+import cn.lzy.mybatiscatalog.Article;
+import cn.lzy.mybatiscatalog.ArticleMapper;
 import cn.lzy.mybatiscatalog.Comment;
 import cn.lzy.mybatiscatalog.CommentMapper;
+import cn.lzy.mybatiscatalog.redis.Address;
+import cn.lzy.mybatiscatalog.redis.Family;
 import cn.lzy.profielconfig.DBConnector;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +21,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 //import sun.java2d.cmm.Profile;
 
 @RunWith(SpringRunner.class)
@@ -46,6 +54,27 @@ public class ChapterTestApplication {
 
     @Autowired
    CommentMapper commentMapper;
+
+    /*@Autowired
+    private DruidDataSource dataSource;
+
+    @Test
+    public void testDruidDataSource() {
+
+        System.out.println(dataSource.getMaxActive());
+    }
+
+    @Autowired
+   private ArticleMapper articleMapper;
+
+    @Test
+    public void selectArticle() {
+
+        Article article = articleMapper.selectArticle(1);
+
+        System.out.println(article);
+
+    }*/
     @Test
     public void contextLoads() {
         System.out.println("单元测试返回的数据--" + chapterController.sayHello());
@@ -56,8 +85,9 @@ public class ChapterTestApplication {
     System.out.println("单元测试返回的customproperties数据--" +customProperties);
         System.out.println("单元测试返回的profile环境--" + dbConnector.config());
         System.out.println("查询数据库" + commentMapper.findById(1));
-       // System.out.println("查询数据库" + commentMapper.findById(1));
-
+//        System.out.println("删除数据成功" + commentMapper.deleteComment(1));
+//        System.out.println("插入数据" + commentMapper.insertComment(id='1', "很全、很详细", "狂奔的蜗牛", '1'));
+//'1', '很全、很详细', '狂奔的蜗牛', '1'
         /*MyProperties myProperties=(MyProperties) applicationContext.getBean("myProperties");
         if (myProperties!=null){
             System.out.println("MyPropertie类注入不成功");
@@ -69,4 +99,23 @@ public class ChapterTestApplication {
 //        String result=chapterController.sayHello();
 //        System.out.println(TAG+"单元测试返回==="+result);
 //    }
+/*@Test
+    public void saveRedisPerson(){
+        cn.lzy.mybatiscatalog.redis.Person personRedis=new cn.lzy.mybatiscatalog.redis.Person();
+        personRedis.setFirstname("张");
+        personRedis.setLastname("三");
+    Address address=new Address();
+    address.setCity("柳州");
+    address.setCountry("中国");
+personRedis.setAddress(address);
+    List<Family> list=new ArrayList<>();
+    Family dad=new Family("父亲","刘");
+    Family mom=new Family("母亲","王");
+    list.add(dad);
+    list.add(mom);
+    personRedis.setFamilyList(list);
+    cn.lzy.mybatiscatalog.redis.Person personResult=repository.save(personRedis);
+    System.out.println(personResult);
+    }*/
+
 }
